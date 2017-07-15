652702ab5cda2880312aa21783e10eb0948a2ee4
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-07-15 07:09:21 +0530

    2.4.0

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bundle install --path vendor/bundle
-bundle exec vendor/bundle/ruby/2.3.0/bin/jekyll serve --incremental -D
+bundle exec vendor/bundle/ruby/2.4.0/bin/jekyll serve --incremental -D
