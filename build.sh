8703c19f8fccca57f9ca35a8f7d5467e09df3c95
Baiju Muthukadan <baiju.m.mail@gmail.com>
2025-05-29 22:50:39 +0530

    Remove old blog

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-bundle install
-bundle exec vendor/bundle/ruby/3.1.0/bin/jekyll build
-cp _site/* ../output/ -R
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bundle install
-bundle exec vendor/bundle/ruby/3.1.0/bin/jekyll serve --incremental -D
